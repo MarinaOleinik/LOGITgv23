99c932fd004bedfce7ebefefe539fd65c7e836ea
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-01-10 14:48:23 +0200

    Vigade otsind on valmis

--- Vigade_otsing.py
+++ Vigade_otsing.py
@@ -1,28 +1,28 @@
 from math import * 
 print("Ruudu karakteristikud")
-a=input('Sisesta ruudu külje pikkus => ')
+a=float(input('Sisesta ruudu külje pikkus => '))
 S=a**2
-print("Ruudu pindala", S)
+print("Ruudu pindala", round(S,1))
 P=4*a
-print("Ruudu ümbermõõt'', P)
-di=a*math.sqr(2)
+print("Ruudu ümbermõõt", round(P,1))
+di=a*sqrt(2)
 print("Ruudu diagonaal", round(di,2))
 print()
-print("Ristküliku karakteristikud"))
-b=input("Sisesta ristküliku 1. külje pikkus => ")
-c=input("Sisesta ristküliku 2. külje pikkus => ")
+print("Ristküliku karakteristikud")
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))
 S=b*c
-print(Ristküliku pindala', S)
-P=2(b+c)
+print('Ristküliku pindala', S)
+P=2*(b+c)
 print("Ristküliku ümbermõõt", P)
-di=math.sqrt(b*2+c*2)
-print("Ristküliku diagonaal", round(di)
+di=sqrt(b**2+c**2)
+print("Ristküliku diagonaal", round(di,1))
 print()
 print("Ringi karakteristikud")
-r=input(''Sisesta ringi raadiusi pikkus => ''))
-d=2r
-print("Ringi läbimõõt" d)
-S=pi()*r*2
-print("Ringi pindala", round(S))
-C=2pi()*r
-print("Ringjoone pikkus", round(C)
+r=float(input('Sisesta ringi raadiusi pikkus => '))
+d=2*r
+print("Ringi läbimõõt", d)
+S=pi*r**2
+print("Ringi pindala", round(S,2))
+C=2*pi*r
+print("Ringjoone pikkus", round(C,2))
