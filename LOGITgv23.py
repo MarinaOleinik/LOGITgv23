e1cdda2a563fa8ab4f9269069e4bdf46c5ac2560
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-01-10 09:57:01 +0200

    vigate otsing ülesanne

--- LOGITgv23.py
+++ LOGITgv23.py
@@ -1,20 +1,36 @@
+from random import * #kõik funktsioonid mis on moodulis
+#7
+a1=randint(1,50)
+a2=randint(1,50)
+a1=randint(1,50)
+a1=randint(1,50)
+a1=randint(1,50)
 
-#print("Tere maailm!")
-#nimi=input("Mis on sinu nimi?")
-#vanus=int(input("Kui vana sa oled?"))# kommentaar
 
-#print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana.")
-#print("Tere",nimi,"! Sa oled",vanus,"aastat vana.")
-#print("Tere {0}! Sa oled {1} aasta vana".format(nimi,vanus))
+
+
+#8
+print("@..@".center(10))
+print("(----)".center(10))
+print("( \__/ )".center(10))
+print('^^ "" ^^'.center(10)) 
+
+print("Tere maailm!")
+nimi=input("Mis on sinu nimi?")
+vanus=int(input("Kui vana sa oled?"))# kommentaar
+
+print("Tere "+nimi+"! Sa oled "+str(vanus)+" aastat vana.") #22!="22"
+print("Tere",nimi,"! Sa oled",vanus,"aastat vana.")
+print("Tere {0}! Sa oled {1} aasta vana".format(nimi,vanus))
 
 #print(type(nimi))
 #print(type(vanus))
 
-#arv1=float(input("Arv 1: "))
-#t=input("Tehe: ")
-#arv2=float(input("Arv 2: "))
-#vastus=eval(str(arv1)+t+str(arv2))
-#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+arv1=float(input("Arv 1: ")) #3 -> 3.0
+t=input("Tehe: ")
+arv2=float(input("Arv 2: "))
+vastus=eval(str(arv1)+t+str(arv2))
+print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 
 #2
 vanus = 18
@@ -27,7 +43,7 @@ print("Muutuja",pikkus,"on",type(pikkus))
 print("Muutuja",kas_käib_koolis,"on",type(kas_käib_koolis))
 
 #3
-from random import * #kõik funktsioonid mis on moodulis
+
 kogus=randint(1,100) #66
 print("Kokku on",kogus,"kommid")
 mitu=int(input("Mitu kommi tahad võtta?")) #33
@@ -37,5 +53,6 @@ print("Laua peal on",kogus,"kommid")
 #4
 from math import *
 l=float(input("Ümbermõõt: "))
-d=round(l/pi,2)          #3.14
+d=round(l/pi,2)          #3.14...
 print("Läbimõõt=", d)
+
---
+++ Vigade_otsing.py
@@ -0,0 +1,28 @@
+from math import * 
+print("Ruudu karakteristikud")
+a=input('Sisesta ruudu külje pikkus => ')
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print("Ruudu ümbermõõt'', P)
+di=a*math.sqr(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud"))
+b=input("Sisesta ristküliku 1. külje pikkus => ")
+c=input("Sisesta ristküliku 2. külje pikkus => ")
+S=b*c
+print(Ristküliku pindala', S)
+P=2(b+c)
+print("Ristküliku ümbermõõt", P)
+di=math.sqrt(b*2+c*2)
+print("Ristküliku diagonaal", round(di)
+print()
+print("Ringi karakteristikud")
+r=input(''Sisesta ringi raadiusi pikkus => ''))
+d=2r
+print("Ringi läbimõõt" d)
+S=pi()*r*2
+print("Ringi pindala", round(S))
+C=2pi()*r
+print("Ringjoone pikkus", round(C)
