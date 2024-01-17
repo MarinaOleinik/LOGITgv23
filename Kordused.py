c86f4e252856c99d23f2c6ac8746d526b5a5ae97
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-01-17 16:30:06 +0200

    Kordused

--- Kordused.py
+++ Kordused.py
@@ -1,12 +1,28 @@
 #1
+print("1. variant")
+k=0
 mitu=0
-for k in range(1,6):
-    n=float(input("Sisesta "+str(k)+". arv"))
+while k<15:
+    k+=1
+    n=float(input("Sisesta arv nr."+str(k)))
+    if int(n)==float(n):
+        mitu+=1   
+print("2. variant")
+k=0
+mitu=0
+while True:
+    k+=1
+    n=float(input("Sisesta arv nr."+str(k)))
+    if int(n)==float(n):
+        mitu+=1
+    if k==15: break
+print("3. variant")
+mitu=0
+for k in range(1,16):
+    n=float(input("Sisesta arv nr."+str(k)))
     if int(n)==float(n):
         mitu+=1
 print("Täisarvude kogus: ",mitu)
-
-
 
 
 
