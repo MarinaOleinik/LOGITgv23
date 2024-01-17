a7fb538ef6a3a334ec5997e0754b73d248535cff
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-01-17 09:52:08 +0200

    Tsüklid

--- Kordused.py
+++ Kordused.py
@@ -1,8 +1,14 @@
-while True:
+while True: #lõpmatu tsükkel
     try:
         pikkus=int(input("Pikkus: "))
         laius=int(input("Laius: "))
+        if pikkus>0 and laius>0:
+            print("Pindala:",pikkus*laius)
+            print("Pikkus ja laius on sisetanud ja pindala on leidnud")
+        else:
+            print("On vaja andmeid suurem kui 0")
+            print("Pikkus ja laius on sisetanud ja pindala ei ole leidnud")
         break
     except :
-        print("On vaja int formaat kasutada!")
-
+        print("On vaja int formaat kasutada!") #ilmub ekraanil, kui viga andmetega
+print("Töö lõpp")
