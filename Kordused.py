cefbb12c728e82d519b6f23049320ad89eb87625
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2024-01-31 14:16:42 +0200

    Sirukus

--- Kordused.py
+++ Kordused.py
@@ -1,5 +1,85 @@
 from datetime import *
 from random import *
+#Praktiline töö
+#Ülesanne 5
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"0",end="")
+    print()
+#Ülesanne 4
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i}*{j}={i*j}")
+#Ülesanne 3
+k=0
+while True:
+    k+=1
+    print(f"{k}. ülesanne")
+    a=randint(1,50)
+    b=randint(1,50)
+    p=5
+    while True:
+        v=int(input(f"{a}+{b}= "))
+        p-=1
+        if v==a+b:
+            print("Õige vastus!")
+            break
+        elif p==0:
+            print(f"{a}+{b}={a+b}")
+            break
+    if k==10: break
+
+
+
+
+
+#Ülesanne 2.2 sõne lõpeb tsükkel
+sum_=0
+i=0
+while True:
+    i+=1
+    arv=input("2.2 Sisesta arv: ")
+    if i>10: break
+    try:
+        arv=int(arv)
+        sum_+=arv
+    except:
+        break
+if sum_!=0:
+    print(f"Summa= {sum_}")
+
+#Ülesanne 2.1 arv 777 lõpeb tsükkel
+sum_=0
+i=0
+while True:
+    i+=1
+    arv=int(input("2.1 Sisesta arv: "))
+    if i>10: break
+    if arv==777: break
+    sum_+=arv
+print(f"Summa= {sum_}")
+
+
+#Ülesanne 2
+sum_=0
+for i in range(10):
+    arv=int(input("Sisesta arv: "))
+    sum_+=arv
+print(f"Summa= {sum_}")
+
+
+
+#Ülesanne 1
+nimi=input("Palun sisesta oma nimi:")
+mitu=int(input("Mitu korda tervitada? "))
+for i in range(mitu):
+    print(f"Ole tervitatud, {nimi}, {i+1}. korda.")
+
+
+
+
+
+
 #8 Poes
 arve_nr= date.today()#datetime.now()
 tsekk="Arve: "+str(arve_nr)+"\nToode Hind Kogus Summa\n"
